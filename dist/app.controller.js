c68095c5f630bcfd512ea437bd9079cbae379779
Nhu Y <tnhuy2000@gmail.com>
2026-04-15 14:39:52 +0700

    fix: test check health

--- dist/app.controller.js
+++ dist/app.controller.js
@@ -36,7 +36,7 @@ __decorate([
     __metadata("design:returntype", String)
 ], AppController.prototype, "getHello", null);
 __decorate([
-    (0, common_1.Get)('health'),
+    (0, common_1.Get)('/health'),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", []),
     __metadata("design:returntype", void 0)
